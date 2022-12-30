e4c7304873df720a36c9a806f467de445824d176
mckrava <mckrava2@gmail.com>
2022-12-30 15:23:51 +0200

    feat: remove typeorm-cache-store

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
-# Multichain Caller Squid 
+# Multichain Caller Squid
 
-### Supported cjains:
+### Supported chains:
+
 - acala
 - bifrost
 - crust
 - gmordie
 - karura
-- kusama  (`sqd deploy https://github.com/subsquid/caller-squid\#multi-chain -m manifest/squid-kusama.yaml`)
+- kusama (`sqd deploy https://github.com/subsquid/caller-squid\#multi-chain -m manifest/squid-kusama.yaml`)
 - moonbeam (`sqd deploy https://github.com/subsquid/caller-squid\#multi-chain -m manifest/squid-moonbeam.yaml`)
 - moonriver (`sqd deploy https://github.com/subsquid/caller-squid\#multi-chain -m manifest/squid-moonriver.yaml`)
 - phala
 - polkadot (`sqd deploy https://github.com/subsquid/caller-squid\#multi-chain -m manifest/squid-polkadot.yaml`)
 
+## Custom migration with GIN indexes for args fields
 
+```javascript
+module.exports = class Data1672344080480 {
+  name = 'Data1672344080480';
+
+  async up(db) {
+    await db.query(`CREATE EXTENSION pg_trgm `);
+    await db.query(`CREATE INDEX "event_args_str_gin" ON "event" USING GIN ("args_str") `);
+    await db.query(`CREATE INDEX "call_args_str_gin" ON "call" USING GIN ("args_str") `);
+    await db.query(`CREATE INDEX "event_args_json_gin" ON "event" USING GIN ("args_json") `);
+    await db.query(`CREATE INDEX "call_args_json_gin" ON "call" USING GIN ("args_json") `);
+  }
+
+  async down(db) {
+    await db.query(`DROP INDEX "public"."event_args_str_gin"`);
+    await db.query(`DROP INDEX "public"."call_args_str_gin"`);
+    await db.query(`DROP INDEX "public"."event_args_json_gin"`);
+    await db.query(`DROP INDEX "public"."call_args_json_gin"`);
+  }
+};
+```
--- src/processor.ts
+++ src/processor.ts
@@ -1,6 +1,6 @@
 import { getConfig } from './config';
 import { lookupArchive } from '@subsquid/archive-registry';
-import { TypeormDatabase } from '@subsquid/processor-tools';
+import { TypeormDatabase } from '@subsquid/typeorm-store';
 import { Block as BlockEntity, Call, Event, Extrinsic } from './model';
 import {
   BatchProcessorCallItem,
@@ -16,7 +16,6 @@ const processor = new SubstrateBatchProcessor()
       release: 'FireSquid'
     })
   })
-  .setBlockRange({ from: 10000000, to: 10100000 })
   .addEvent('*', {
     data: {
       event: {
@@ -39,154 +38,149 @@ const processor = new SubstrateBatchProcessor()
 
 type CallItem = BatchProcessorCallItem<typeof processor>;
 
-processor.run(
-  new TypeormDatabase({
-    disableAutoFlush: true,
-    disableAutoTxCommit: true,
-    disableAutoHeightUpdate: true
-  }),
-  async (ctx) => {
-    for (let block of ctx.blocks) {
-      const currentBlock = new BlockEntity({
-        id: block.header.id,
-        height: block.header.height,
-        hash: block.header.hash,
-        parentHash: block.header.parentHash,
-        timestamp: new Date(block.header.timestamp)
-      });
+processor.run(new TypeormDatabase(), async (ctx) => {
+  const entitiesStore = new Map<
+    'block' | 'event' | 'call' | 'extrinsic',
+    Map<string, BlockEntity | Event | Call | Extrinsic>
+  >();
+  entitiesStore.set('block', new Map());
+  entitiesStore.set('event', new Map());
+  entitiesStore.set('call', new Map());
+  entitiesStore.set('extrinsic', new Map());
 
-      ctx.store.deferredUpsert(currentBlock);
+  for (let block of ctx.blocks) {
+    const currentBlock = new BlockEntity({
+      id: block.header.id,
+      height: block.header.height,
+      hash: block.header.hash,
+      parentHash: block.header.parentHash,
+      timestamp: new Date(block.header.timestamp)
+    });
 
-      for (let item of block.items) {
-        switch (item.kind) {
-          case 'event': {
-            const { id, name, indexInBlock, extrinsic, args } = item.event;
+    entitiesStore.get('block')!.set(currentBlock.id, currentBlock);
 
-            const decoratedName = name.split('.');
+    for (let item of block.items) {
+      switch (item.kind) {
+        case 'event': {
+          const { id, name, indexInBlock, extrinsic, args } = item.event;
 
-            const newEvent = new Event({
-              id,
-              block: currentBlock,
-              blockNumber: currentBlock.height,
-              timestamp: currentBlock.timestamp,
-              indexInBlock: indexInBlock ?? null,
-              palletName: decoratedName[0],
-              eventName: decoratedName[1]
-            });
+          const decoratedName = name.split('.');
 
+          const newEvent = new Event({
+            id,
+            block: currentBlock,
+            blockNumber: currentBlock.height,
+            timestamp: currentBlock.timestamp,
+            indexInBlock: indexInBlock ?? null,
+            palletName: decoratedName[0],
+            eventName: decoratedName[1]
+          });
+
+          try {
+            newEvent.argsStr = getParsedArgs(args);
+            // newEvent.argsJson = newEvent.argsStr;
+          } catch (e) {
+            ctx.log.warn('Event args cannot be stringified.');
+            console.dir(e, { depth: null });
+          }
+
+          if (extrinsic) {
+            // @ts-ignore
+            newEvent.extrinsic = { id: extrinsic.id };
+            newEvent.extrinsicHash = extrinsic.hash;
+            // @ts-ignore
+            newEvent.call = { id: extrinsic.call.id };
+          }
+          entitiesStore.get('event')!.set(newEvent.id, newEvent);
+          break;
+        }
+        case 'call': {
+          const { extrinsic }: CallItem = item;
+          let signer: string | null = null;
+          let encodedSignerAccount: string | null = null;
+          const decoratedCallName = item.call.name.split('.');
+
+          if (
+            extrinsic.signature &&
+            extrinsic.signature.address &&
+            extrinsic.signature.address.__kind &&
+            extrinsic.signature.address.__kind === 'Id'
+          ) {
+            signer = extrinsic.signature.address.value;
+            encodedSignerAccount = encodeAccount(
+              signer,
+              chainConfig.srcConfig.prefix
+            );
+          }
+
+          const newExtrinsic = new Extrinsic({
+            id: item.extrinsic.id,
+            block: currentBlock,
+            blockNumber: currentBlock.height,
+            timestamp: currentBlock.timestamp,
+            extrinsicHash: extrinsic.hash,
+            indexInBlock: extrinsic.indexInBlock,
+            version: extrinsic.version,
+            signerPublicKey: signer,
+            signerAccount: encodedSignerAccount,
+            error: extrinsic.error ? JSON.stringify(extrinsic.error) : null,
+            success: extrinsic.success,
+            tip: extrinsic.tip,
+            fee: extrinsic.fee
+          });
+
+          const newCall = new Call({
+            id: item.call.id,
+            palletName: decoratedCallName[0],
+            callName: decoratedCallName[1],
+            parentId: item.call.parent ? item.call.parent.id : null,
+            blockNumber: currentBlock.height,
+            timestamp: currentBlock.timestamp,
+            block: currentBlock,
+            extrinsic: newExtrinsic,
+            extrinsicHash: newExtrinsic.extrinsicHash,
+            success: extrinsic.success,
+            callerPublicKey: signer,
+            callerAccount: encodedSignerAccount
+          });
+
+          if (
+            // @ts-ignore
+            item.call.name !== 'Utility.batch' &&
+            // @ts-ignore
+            item.call.name !== 'Utility.batch_all'
+          ) {
             try {
-              newEvent.argsStr = getParsedArgs(args);
-              // newEvent.argsJson = newEvent.argsStr;
+              newCall.argsStr = getParsedArgs(item.call.args);
+              // newCall.argsJson = newCall.argsStr;
             } catch (e) {
-              ctx.log.warn('Event args cannot be stringified.');
+              ctx.log.warn(
+                `Event args cannot be stringified in call ${item.call.id}.`
+              );
               console.dir(e, { depth: null });
             }
-
-            if (extrinsic) {
-              // @ts-ignore
-              newEvent.extrinsic = { id: extrinsic.id };
-              newEvent.extrinsicHash = extrinsic.hash;
-              // @ts-ignore
-              newEvent.call = { id: extrinsic.call.id };
-            }
-
-            ctx.store.deferredUpsert(newEvent);
-            break;
           }
-          case 'call': {
-            const { extrinsic }: CallItem = item;
-            let signer: string | null = null;
-            let encodedSignerAccount: string | null = null;
-            const decoratedCallName = item.call.name.split('.');
 
-            if (
-              extrinsic.signature &&
-              extrinsic.signature.address &&
-              extrinsic.signature.address.__kind &&
-              extrinsic.signature.address.__kind === 'Id'
-            ) {
-              signer = extrinsic.signature.address.value;
-              encodedSignerAccount = encodeAccount(
-                signer,
-                chainConfig.srcConfig.prefix
-              );
-            }
+          entitiesStore.get('call')!.set(newCall.id, newCall);
+          entitiesStore.get('extrinsic')!.set(newExtrinsic.id, newExtrinsic);
 
-            const newExtrinsic = new Extrinsic({
-              id: item.extrinsic.id,
-              block: currentBlock,
-              blockNumber: currentBlock.height,
-              timestamp: currentBlock.timestamp,
-              extrinsicHash: extrinsic.hash,
-              indexInBlock: extrinsic.indexInBlock,
-              version: extrinsic.version,
-              signerPublicKey: signer,
-              signerAccount: encodedSignerAccount,
-              error: extrinsic.error ? JSON.stringify(extrinsic.error) : null,
-              success: extrinsic.success,
-              tip: extrinsic.tip,
-              fee: extrinsic.fee
-            });
-
-            const newCall = new Call({
-              id: item.call.id,
-              palletName: decoratedCallName[0],
-              callName: decoratedCallName[1],
-              parentId: item.call.parent ? item.call.parent.id : null,
-              blockNumber: currentBlock.height,
-              timestamp: currentBlock.timestamp,
-              block: currentBlock,
-              extrinsic: newExtrinsic,
-              extrinsicHash: newExtrinsic.extrinsicHash,
-              success: extrinsic.success,
-              callerPublicKey: signer,
-              callerAccount: encodedSignerAccount
-            });
-
-            if (
-              // @ts-ignore
-              item.call.name !== 'Utility.batch' &&
-              // @ts-ignore
-              item.call.name !== 'Utility.batch_all'
-            ) {
-              try {
-                newCall.argsStr = getParsedArgs(item.call.args);
-                // newCall.argsJson = newCall.argsStr;
-              } catch (e) {
-                ctx.log.warn(
-                  `Event args cannot be stringified in call ${item.call.id}.`
-                );
-                console.dir(e, { depth: null });
-              }
-            }
-
-            ctx.store.deferredUpsert(newExtrinsic);
-            ctx.store.deferredUpsert(newCall);
-
-            break;
-          }
-          default:
+          break;
         }
+        default:
       }
     }
-
-    const saveThreshold =
-      process.env.SAVE_THRESHOLD ?? chainConfig.batchSizeSaveThreshold;
-    if (
-      ctx.blocks.length === 1 ||
-      [...ctx.store.values(BlockEntity)].length > saveThreshold
-    ) {
-      ctx.log.info(
-        `------------ batch size ${ctx.blocks.length} :: Saved: ${
-          [...ctx.store.values(BlockEntity)].length
-        } Blocks | ${[...ctx.store.values(Extrinsic)].length} extrinsics | ${
-          [...ctx.store.values(Call)].length
-        } calls | ${[...ctx.store.values(Event)].length} events ------------ `
-      );
-
-      await ctx.store.flush();
-      ctx.store.purge();
-      await ctx.store.UNSAFE_commitTransaction();
-    }
   }
-);
+
+  if (entitiesStore.get('block')!.size > 0)
+    await ctx.store.insert([...entitiesStore.get('block')!.values()]);
+
+  if (entitiesStore.get('extrinsic')!.size > 0)
+    await ctx.store.insert([...entitiesStore.get('extrinsic')!.values()]);
+
+  if (entitiesStore.get('call')!.size > 0)
+    await ctx.store.insert([...entitiesStore.get('call')!.values()]);
+
+  if (entitiesStore.get('event')!.size > 0)
+    await ctx.store.insert([...entitiesStore.get('event')!.values()]);
+
+});

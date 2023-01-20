49aac9176b5451db3ff4e1b31f869990506cb32e
Mikhail Shulgin <mashulgin@edu.hse.ru>
2023-01-20 18:37:44 +0300

    impl: new indexes

--- db/migrations/1873961028900-Indexes.js
+++ db/migrations/1873961028900-Indexes.js
@@ -4,12 +4,10 @@ module.exports = class Indexes1873961028900 {
   async up(db) {
     await db.query(`CREATE INDEX "event_args_str_gin" ON "event" USING GIN ("args_str") `);
     await db.query(`CREATE INDEX "call_args_str_gin" ON "call" USING GIN ("args_str") `);
-    await db.query(`CREATE INDEX "call_name_pallet_parent" ON "call" ("call_name", "pallet_name", "parent_id") `)
   }
 
   async down(db) {
     await db.query(`DROP INDEX "public"."event_args_str_gin"`);
     await db.query(`DROP INDEX "public"."call_args_str_gin"`);
-    await db.query(`DROP INDEX "public"."call_name_pallet_parent"`);
   }
 };
